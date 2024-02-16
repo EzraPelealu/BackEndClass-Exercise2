fbac7286655187fba851fb50cfb28a6ae0d1aebc
Ezra Pelealu <116333571+EzraPelealu@users.noreply.github.com>
2024-02-16 12:25:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 const express = require('express');
 const morgan = require('morgan');
-const members = require('./members');
 const users = require('./users');
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.status(200).send('This is the homepage');
-});
-
-app.get('/about', (req, res) => {
-  const response = {
-    status: 'success',
-    massage: 'response success',
-    description: 'Exercise #03',
-    date: new Date().toISOString(),
-    data: members
-  };
-  res.status(210).json(response);
-});
-
+// Endpoint untuk mendapatkan list data users
 app.get('/users', (req, res) => {
-  const response = {
-    status: 'success',
-    massage: 'response success',
-    description: 'Exercise #03',
-    date: new Date().toISOString(),
-    data: users
-  };
-  res.status(220).json(response);
+    res.json(users);
+});
+
+// Endpoint untuk mendapatkan data user berdasarkan nama
+app.get('/users/:name', (req, res, next) => {
+    const { name } = req.params;
+    const user = users.find(user => user.name.toLowerCase() === name.toLowerCase());
+    if (!user) {
+      const error = new Error('Data user tidak ditemukan');
+      error.status = 404;
+      return next(error);
+    }
+    res.json(user);
+});
+
+// Middleware untuk menangani error 404
+app.use((req, res, next) => {
+  const error = new Error('Resource tidak ditemukan');
+  error.status = 404;
+  next(error);
+});
+
+// Middleware untuk menangani error lainnya
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    status: 'error',
+    message: err.message || 'Terjadi kesalahan pada server',
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
